192fdb93ed0401b59e0b86e3a9bf2c7a2b374589
jessicahattig <jessicahattig@gmail.com>
2023-07-11 19:33:24 -0700

    BUGFIX: Function 1, test 1, pass.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 //Business Logic - Player
 
-function Player(name, score) {
-  this.name = name;
-  this.score = 0;
-}
+class Player {
+  constructor(name, score) {
+    this.name = name;
+    this.score = 0;
+  }
+};
 
 Player.prototype.equals = function (otherPlayer) {
   return this.name === otherPlayer.name;
@@ -13,25 +15,26 @@ Player.prototype.setScoreToZero = function () {
   this.score = 0;
 };
 
+
 //Business Logic - Game
-function Game() {
+class Game() {
   this.players = [];
 };
 
 Game.prototype.addPlayer = function (player) {
- if (this.players.length < 2) {
-  this.players.push(player);
-  player.setScoreToZero();
- }
+  if (this.players.length < 2) {
+    this.players.push(player);
+    player.setScoreToZero();
+  }
 };
 
 Game.prototype.roll = function () {
   const randomNumber = Math.floor(Math.random() * 6) + 1;
   const currentPlayer = this.players[0];
-    if (randomNumber !== 1) {
-      currentPlayer.score += randomNumber;
-    }
-    return currentPlayer.score;
+  if (randomNumber !== 1) {
+    currentPlayer.score += randomNumber;
+  }
+  return currentPlayer.score;
 };
 
 
